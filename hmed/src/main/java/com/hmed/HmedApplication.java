203a481b36eb06b34ed6d2796464e37cd0bcd94e
yoojun2074 <yoojun2074@naver.com>
2024-07-27 17:25:07 +0900

    업로드 제발

---
+++ hmed/src/main/java/com/hmed/HmedApplication.java
@@ -0,0 +1,13 @@
+package com.hmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmedApplication.class, args);
+	}
+
+}
---
+++ hmed/src/test/java/com/hmed/HmedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
